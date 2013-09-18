2ff5a99ae7bc8678e7aa08be74e03aab3409a14b
Michael Smith <sitecross@gmail.com>
2013-09-17 19:35:29 -0500

    add getHumanReadableTime() to interval

--- src/MS/Throttle/Interval.php
+++ src/MS/Throttle/Interval.php
@@ -22,4 +22,24 @@ class Interval {
         $this->count = $count;
     }
 
+    /**
+     * @return string
+     */
+    public function getHumanReadableTime()
+    {
+        if($this->seconds < 60){
+            return sprintf('%d seconds', $this->seconds);
+        }elseif($this->seconds < 3600){
+            return sprintf('%d minutes', $this->seconds / 60);
+        }elseif($this->seconds < 86400){
+            return sprintf('%d hours', $this->seconds / 3600);
+        }elseif($this->seconds < 2592000){
+            return sprintf('%d days', $this->seconds / 86400);
+        }elseif($this->seconds < 31536000){
+            return sprintf('%d months', $this->seconds / 2592000);
+        }else{
+            return sprintf('%d years', $this->seconds / 31536000);
+        }
+    }
+
 }
